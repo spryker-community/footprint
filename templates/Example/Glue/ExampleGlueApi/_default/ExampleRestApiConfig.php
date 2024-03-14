1ca855aae46dbe6984fc4abeacef43a896a9858f
jadoon88 <umair@merrycode.com>
2024-03-14 10:28:46 +0100

    add template brainstorm notes

---
+++ templates/Example/Glue/ExampleGlueApi/_default/ExampleRestApiConfig.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+
+namespace ExampleGlueApiConfig\_choice1;
+
+use Spryker\Glue\CartsRestApi\CartsRestApiConfig as SprykerCartsRestApiConfig;
+
+class ExampleRestApiConfig extends SprykerCartsRestApiConfig
+{
+    /**
+     * @var bool
+     */
+    protected const ALLOWED_CART_ITEM_EAGER_RELATIONSHIP = false;
+
+    /**
+     * @var bool
+     */
+    protected const ALLOWED_GUEST_CART_ITEM_EAGER_RELATIONSHIP = false;
+}
